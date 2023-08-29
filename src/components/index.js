d2aeba9e8184dbc2a9767391fb18db9cbdd86c0e
Joel-K-James <joelkjames@gmail.com>
2023-08-29 11:50:40 +0530

    first commit

---
+++ src/components/index.js
@@ -0,0 +1,6 @@
+export { default as Navbar } from "./Navbar"
+export { default as Feed } from "./Feed"
+export { default as Search } from "./Search"
+export { default as Video } from "./Video"
+export { default as Channel } from "./Channel"
+export { default as SearchBar } from "./SearchBar"
